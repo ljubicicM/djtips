98f55abb600569e1d56787f985c2a981c531ed08
Marko Ljubičić <markoljubicic77@gmail.com>
2023-12-30 21:45:02 +0100

    qr code component added

---
+++ src/components/QRCodeComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import QRCode from 'react-qr-code';
+
+export default function QRCodeComponent({ data }) {
+    return (
+        <div>
+            <QRCode size={256}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                value={value}
+                viewBox={`0 0 256 256`} />
+        </div>
+    );
+}

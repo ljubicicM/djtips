85f9af9643ed39da58df6ac43cf70fb06d669453
Milos Bobic <bobicm02@gmail.com>
2023-12-30 15:44:47 +0100

    Added TextField component to /components

---
+++ src/components/TextField.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextField = ({style, name, type, placeholder}) => {
+    <input className={style} name={name} type={type} placeholder={placeholder}></input>
+};
+
+export default TextField;
